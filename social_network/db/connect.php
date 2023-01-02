389065458cd629949d11e20011e1c87651d299b8
Pradip Dhungana <100480407+dhunganaPradeep@users.noreply.github.com>
2023-01-02 21:06:50 +0545

    Delete social_network directory

--- social_network/db/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// php code to connect to server localhost
-
-$connection = mysqli_connect("localhost", "root", "");
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
